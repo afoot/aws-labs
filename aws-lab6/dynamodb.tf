743d9bd0bd0b0ea407004cab47b5e0cc6c30c3f0
Ubuntu <ubuntu@ip-172-31-88-29.ec2.internal>
2025-06-23 21:52:48 +0000

    Added iam

--- aws-lab6/dynamodb.tf
+++ aws-lab6/dynamodb.tf
@@ -35,3 +35,28 @@ resource "aws_iam_role" "lambda_dynamodb_role" {
   })
 
 }
+
+resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
+  name = "lambda_dynamodb_policy"
+  role = aws_iam_role.lambda_dynamodb_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+	  "dynamedb:PutItem",
+	  "logs:CreateLogGroup",
+	  "logs:CreateLogStream",
+	  "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*",
+	aws_dynamodb_table.users_table.arn
+      },
+    ]
+  })
+}
